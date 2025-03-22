4943e1b49f302fa758222b3c3422e574329c8eb7
nk-31012002 <102040326+nk-31012002@users.noreply.github.com>
2025-03-22 21:54:13 +0530

    Authentication implemented

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,7 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
+
 export default function Home() {
   return (
-    <p>
-      This is a protected page
-    </p>
+    <div>
+      <UserButton
+        afterSignOutUrl="/"
+      />
+    </div>
   );
 }
