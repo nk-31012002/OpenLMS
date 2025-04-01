79dc6f08cd451aa99062640e72147f84cdbab9ec
nk-31012002 <102040326+nk-31012002@users.noreply.github.com>
2025-04-01 19:38:31 +0530

    search part 1

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -23,6 +23,16 @@ const CourseIdPage = async ({
         }
     });
 
+    const categories = await db.category.findMany({
+        orderBy: {
+            name: "asc",
+        },
+    });
+
+
+
+
+
     if(!course){
         return redirect("/");
     }
